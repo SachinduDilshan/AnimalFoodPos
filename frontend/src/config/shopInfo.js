2d181f9304d7d5840fe776bf5d0a8828dbe5caee
Chelaka Liyanage <chelakalakshitha@gmail.com>
2026-09-24 10:53:03 +0530

    feat(config): update shop details in shopInfo.js

--- frontend/src/config/shopInfo.js
+++ frontend/src/config/shopInfo.js
@@ -2,6 +2,6 @@
 // Hardcoded for now since there's no /api/settings endpoint yet to source
 // this from shop_name/address/phone in the settings table - once that
 // exists, this file should be replaced by a fetch, not just new constants.
-export const SHOP_NAME = 'Your Shop Name'
-export const SHOP_ADDRESS = 'Your Shop Address, City'
-export const SHOP_PHONE = '0XX-XXXXXXX'
+export const SHOP_NAME = 'SATHTHAR FEED'
+export const SHOP_ADDRESS = 'No. 52, Hiripitiya Road, Wellawa.'
+export const SHOP_PHONE = '076-26661014 / 077-7781014'
